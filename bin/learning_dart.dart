ea8421defbd124398bcbe47179ad0ea0d0cf627a
Victor Silva <victor.silvaPC@hotmail.com>
2023-04-22 15:27:59 -0300

    :tada: learing lists

--- bin/learning_dart.dart
+++ bin/learning_dart.dart
@@ -1,11 +1,11 @@
 // import 'modules/variables.dart' as variables;
-// import 'modules/lists.dart' as lists;
+import 'modules/lists.dart' as lists;
 // import 'modules/operators.dart' as operators;
-import 'modules/metadata.dart' as metadata;
+// import 'modules/metadata.dart' as metadata;
 
 void main(List<String> args) {
   // variables.run();
-  // lists.run();
+  lists.run();
   // operators.run();
-  metadata.run();
+  // metadata.run();
 }
--- bin/modules/lists.dart
+++ bin/modules/lists.dart
@@ -3,8 +3,86 @@ void run() {
 
   List<String> names = ["Victor", "Caio", "Lincon"];
 
-  // Filtering the List;
-  var newList1 = names.where((element) => element.endsWith("o")).toList();
+  print("Names: $names");
 
-  print(newList1);
+  // const list
+
+  var constantList = const [1, 2, 3];
+
+  // constantList[0] = 1; This will throw an error.
+
+  print(constantList);
+
+  var spreadOperator = [...names, ...?constantList];
+
+  print("Spread Operator: $spreadOperator");
+
+  var nav = [
+    'Home',
+    'Furniture',
+    'Plants',
+    if (true) 'Outlet'
+  ]; // it will adds Outlet
+
+  print(nav);
+
+  var listOfInts = [1, 2, 3];
+  var listOfStrings = ['#0', for (var i in listOfInts) '#$i'];
+
+  print(listOfStrings);
+
+  // SETS
+
+  /// sets can only have unique elements which means there are no copies
+
+  var halogens = {
+    'fluorine',
+    'chlorine',
+    'bromine',
+    'iodine',
+    'astatine',
+  };
+
+  print(halogens);
+
+  var set2 = <String>{};
+
+  set2
+    ..add('fluorine')
+    ..addAll(halogens);
+
+  print(set2);
+
+  // Maps
+
+  /**
+   * In general, a map is an object that associates keys and values. 
+   * Both keys and values can be any type of object. 
+   * Each key occurs only once, but you can use the same value multiple times. Dart support for 
+   * maps is provided by map literals and the Map type.
+   */
+
+  var gifts = {
+    // Key:    Value
+    'first': 'partridge',
+    'second': 'turtledoves',
+    'fifth': 'golden rings'
+  }; // will have the type Map<String, String>
+
+  var nobleGases = {
+    2: 'helium',
+    10: 'neon',
+    18: 'argon',
+  }; // will have the tile Map<int, String>
+
+  print(gifts["fifth"]);
+  print(nobleGases[10]);
+
+  var newMap = Map<String, dynamic>();
+
+  newMap["victor"] = {"role": "Web Developer", "age": 18};
+
+  newMap["Lincon"] = {"role": "Video Editor", "age": 18};
+
+  print(newMap["victor"]["role"]); // will return "Web Developer"
 }
